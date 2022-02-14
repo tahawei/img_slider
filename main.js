1a8aaea0295499305ce3ba6a423baf66981b2283
TAHAWE <amer.zp582@gmail.com>
2022-02-14 22:17:19 +0200

    first commit

--- main.js
+++ main.js
@@ -20,6 +20,30 @@ let prevButton = document.getElementById("prev");
 nextButton.onclick = nextSlide;
 prevButton.onclick = prevSlide;
 
+// Create The Main Ul Element
+let paginationElement = document.createElement("ul");
+
+// Set Id On Created Ul Element
+paginationElement.setAttribute("id", "pagination-ul");
+
+// Create List Items Based On Slides Count
+for (let i = 1; i <= slidesCount; i++) {
+  // Create The Li
+  let paginationItem = document.createElement("li");
+
+  // Set Custom Attribute
+  paginationItem.setAttribute("data-index", i);
+
+  // Set Item Content
+  paginationItem.appendChild(document.createTextNode(i));
+
+  // Append Items To The Main Ul List
+  paginationElement.appendChild(paginationItem);
+}
+
+// Add The Created Ul Element To The Page
+document.getElementById("indicators").appendChild(paginationElement);
+
 // Next Slide Function
 function nextSlide() {
   console.log("Next");
