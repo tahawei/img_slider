508373b91f8acd1491253fcc6e205f71cdec454e
TAHAWE <amer.zp582@gmail.com>
2022-02-16 23:51:15 +0200

    first commit

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let sliderImages = Array.from(
 let slidesCount = sliderImages.length;
 
 // Set Current Slide
-let currnetSlide = 1;
+let currnetSlide = 2;
 
 // Slide Number Element
 let slideNumberElement = document.getElementById("slide-number");
@@ -44,6 +44,10 @@ for (let i = 1; i <= slidesCount; i++) {
 // Add The Created Ul Element To The Page
 document.getElementById("indicators").appendChild(paginationElement);
 
+// Get The New Created Ul
+
+let paginationCreatedUl = document.getElementById("pagination-ul");
+theCheacker();
 // Next Slide Function
 function nextSlide() {
   console.log("Next");
@@ -51,4 +55,16 @@ function nextSlide() {
 // Next Previous Function
 function prevSlide() {
   console.log("Prev");
+}
+
+// Create The Cheacker Function
+function theCheacker() {
+  // Set The Slide Number
+  slideNumberElement.textContent = `Slide #${currnetSlide} of ${slidesCount}`;
+
+  // Set Active Class On Current Slide
+  sliderImages[currnetSlide - 1].classList.add("active");
+
+  // Set Active Class On Current Pagination Item
+  paginationCreatedUl.children[currnetSlide - 1].classList.add("active");
 }
